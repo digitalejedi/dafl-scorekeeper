8e445cb55826908236aa8cffee5ad4039e97ed9f
Simon Malone <simon@malone.dk>
2021-01-03 17:35:58 +0100

    Added more fragment views, working on main game fragment view

--- app/src/main/kotlin/dk/malone/dafl/scorekeeper/fragment/SetupTeamFragment.kt
+++ app/src/main/kotlin/dk/malone/dafl/scorekeeper/fragment/SetupTeamFragment.kt
@@ -5,19 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dk.malone.dafl.scorekeeper.databinding.FragmentSetupMatchBinding
+import dk.malone.dafl.scorekeeper.databinding.FragmentSetupTeamBinding
 
-class SetupMatchFragment : Fragment() {
+class SetupTeamFragment : Fragment() {
 
-    private var _binding: FragmentSetupMatchBinding? = null
-    private val binding: FragmentSetupMatchBinding get() = _binding!!
+    private var _binding: FragmentSetupTeamBinding? = null
+    private val binding: FragmentSetupTeamBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentSetupMatchBinding.inflate(inflater, container, false)
+        _binding = FragmentSetupTeamBinding.inflate(inflater, container, false)
         return binding.root
     }
 
